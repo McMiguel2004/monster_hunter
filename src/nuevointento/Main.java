37abafdb7017446193f2ad2dae2ea91d8678762c
McMiguel2004 <mcgomezalcantara@elpuig.xeill.net>
2024-02-07 22:17:14 +0100

    nuevo metodo de cambiar descripciones

--- src/nuevointento/Main.java
+++ src/nuevointento/Main.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * Clase principal que contiene el método main para la ejecución del programa.
+ */
+
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
